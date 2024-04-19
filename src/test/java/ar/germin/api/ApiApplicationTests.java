c9faf316737e43f0d2206e58df10d8baa1db8cba
Diego Navarrete <diego.navarrete92@gmail.com>
2024-04-19 20:01:53 -0300

    initial commit

---
+++ src/test/java/ar/germin/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.germin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
