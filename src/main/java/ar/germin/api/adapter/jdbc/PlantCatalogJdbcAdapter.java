7a1f19c66fe62f674e39804239d3c401ecd3a912
1Mariano <mariano_ariel_97@outlook.com>
2024-06-27 22:34:54 -0300

    plants catalog ok

--- src/main/java/ar/germin/api/adapter/jdbc/PlantCatalogJdbcAdapter.java
+++ src/main/java/ar/germin/api/adapter/jdbc/PlantCatalogJdbcAdapter.java
@@ -1,6 +1,7 @@
 package ar.germin.api.adapter.jdbc;
 
 import ar.germin.api.adapter.jdbc.models.PlantCatalogModel;
+import ar.germin.api.adapter.jdbc.models.PlantCatalogModelAlternative;
 import ar.germin.api.application.domain.PlantCatalog;
 import ar.germin.api.application.exceptions.ErrorPlantSaveException;
 import ar.germin.api.application.exceptions.PlantCatalogNotFoundException;
@@ -14,21 +15,26 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.ErrorResponseException;
 
+import java.util.List;
+
 
 @Component
 @Slf4j
 public class PlantCatalogJdbcAdapter implements GetPlantCatalogRepository, SavePlantCatalogRepository {
     private static final String SELECT_PLANT_CATALOG_BY_SCIENTIFIC_NAME_PATH = "sql/selectPlantCatalogByScientificName.sql";
     private static final String SAVE_PLANT_CATALOG_PATH = "sql/savePlantCatalog.sql";
+    private static final String GET_PLANTS_CATALOG_PATH = "sql/getPlantsCatalog.sql";
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final String selectPlantCatalogByScientificNameSql;
     private final String savePlantCatalogSql;
+    private final String selectPlantsCatalog;
 
     public PlantCatalogJdbcAdapter(SqlReader sqlReader, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
         this.selectPlantCatalogByScientificNameSql = sqlReader.readSql(SELECT_PLANT_CATALOG_BY_SCIENTIFIC_NAME_PATH);
         this.savePlantCatalogSql = sqlReader.readSql(SAVE_PLANT_CATALOG_PATH);
+        this.selectPlantsCatalog = sqlReader.readSql(GET_PLANTS_CATALOG_PATH);
     }
 
     @Override
@@ -52,6 +58,30 @@ public class PlantCatalogJdbcAdapter implements GetPlantCatalogRepository, SaveP
             log.error("Error querying plant_catalog with scientific name [{}]", slugScientificName, e);
             throw e;
         }
+    }
+
+    @Override
+    public List<PlantCatalog> getPlantsCatalogs(Double minTemperature, Double maxTemperature, String luz, String temporada, Integer squareCentimeters) {
+
+        try {
+            MapSqlParameterSource params = new MapSqlParameterSource()
+                    .addValue("minTemperature", minTemperature)
+                    .addValue("maxTemperature", maxTemperature)
+                    .addValue("luz", luz)
+                    .addValue("temporada", temporada)
+                    .addValue("squareCentimeters", squareCentimeters);
+
+            log.info("querying plant_catalog with sql [{}] with params: [{}]", selectPlantsCatalog , params );
+            List<PlantCatalogModelAlternative> plantCatalogModel =
+                    this.namedParameterJdbcTemplate.query(selectPlantsCatalog, params, BeanPropertyRowMapper.newInstance(PlantCatalogModelAlternative.class));
+
+            return PlantCatalogModelAlternative.toDomainFromList(plantCatalogModel);
+        } catch (EmptyResultDataAccessException e){
+            log.warn("Plant not found with min_temperature {}, max_temperature {}, luz {}, temporada {}", minTemperature, maxTemperature, luz, temporada);
+            throw new PlantCatalogNotFoundException();
+        }
+
+
     }
 
     @Override
--- src/main/java/ar/germin/api/application/usecase/GetPlantCatalogUseCase.java
+++ src/main/java/ar/germin/api/application/usecase/GetPlantCatalogUseCase.java
@@ -1,22 +1,93 @@
 package ar.germin.api.application.usecase;
 
+import ar.germin.api.application.domain.HistoricWeather;
 import ar.germin.api.application.domain.PlantCatalog;
 import ar.germin.api.application.port.in.GetPlantCatalogPortIn;
 import ar.germin.api.application.port.out.GetPlantCatalogRepository;
+import ar.germin.api.application.port.out.GetWeatherRepository;
 import ar.germin.api.application.usecase.service.GetPlantCatalogService;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @Component
 public class GetPlantCatalogUseCase implements GetPlantCatalogPortIn {
     private final GetPlantCatalogService getPlantCatalogService;
+    private final GetWeatherRepository getWeatherRepository;
+    private final GetPlantCatalogRepository getPlantCatalogRepository;
 
 
-    public GetPlantCatalogUseCase(GetPlantCatalogService getPlantCatalogService) {
+    public GetPlantCatalogUseCase(GetPlantCatalogService getPlantCatalogService,
+                                  GetWeatherRepository getWeatherRepository,
+                                  GetPlantCatalogRepository getPlantCatalogRepository) {
         this.getPlantCatalogService = getPlantCatalogService;
+        this.getWeatherRepository = getWeatherRepository;
+        this.getPlantCatalogRepository = getPlantCatalogRepository;
     }
     //nuevo endpoint
     @Override
     public PlantCatalog getPlantCatalog(String scientificName) {
         return this.getPlantCatalogService.getPlantCatalog(scientificName);
     }
+
+    @Override
+    public List<PlantCatalog> getPlantsCatalog(Float latitude, Float longitude, Integer sunExposure, Integer squareMeters) {
+        HistoricWeather weather = this.getWeatherRepository.getWeather(latitude, longitude);
+        String luz = this.getSunLight(sunExposure);
+        String temporada = this.getTemporada(latitude);
+
+        return this.getPlantCatalogRepository
+                .getPlantsCatalogs(weather.getMinTemperature(),
+                        weather.getMaxTemperature(),
+                        luz,
+                        temporada,
+                        squareMeters);
+
+    }
+
+    private String getTemporada(Float latitude) {
+        LocalDate fecha = LocalDate.now();
+        Month mes = fecha.getMonth();
+        if (latitude < 0) {
+            return switch (mes) {
+                case DECEMBER, JANUARY, FEBRUARY -> "Verano";
+                case MARCH, APRIL, MAY -> "Otoño";
+                case JUNE, JULY, AUGUST -> "Invierno";
+                case SEPTEMBER, OCTOBER, NOVEMBER -> "Primavera";
+                default -> "Desconocida";
+            };
+        } else {
+            return "Desconocida";
+        }
+    }
+
+
+
+    private String getSunLight(Integer sunExposure){
+        if (sunExposure >= 1 && sunExposure <= 3) {
+            return "sombra";
+        } else if (sunExposure >= 4 && sunExposure <= 6) {
+            return "sol parcial";
+        } else if (sunExposure >= 7 && sunExposure <= 10) {
+            return "sol pleno";
+        } else {
+            return "valor fuera de rango";
+        }
+    }
+    // temperatura min y max
+    // altura ?
+    // campo: sun light
+    // sombra
+    // sol parcial
+    // sol pleno
+    // 1 3     4 6     7 10
+
+    // campo: width   centimetros
+    // 30         zapallo
+
+    // campo: planting time
+    // las 4 temporadas
+
 }
