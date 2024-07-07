24fcd4917f8ad3028e542de9155072545f530d54
Diego Navarrete <diego.navarrete92@gmail.com>
2024-07-07 20:22:14 -0300

    wip

--- src/main/java/ar/germin/api/adapter/controller/filters/UserWebFilter.java
+++ src/main/java/ar/germin/api/adapter/controller/filters/UserWebFilter.java
@@ -1,10 +1,10 @@
 package ar.germin.api.adapter.controller.filters;
 
+import ar.germin.api.application.domain.User;
 import ar.germin.api.application.port.out.GetUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
@@ -24,19 +24,27 @@ public class UserWebFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-
-        ServerWebExchange ex = exchange.mutate().request(builder -> builder.header("id-user", "32")).build();
-
-        return chain.filter(ex);
-        /*return exchange.getPrincipal()
-                .flatMap(principal -> {
+        return exchange
+                .getPrincipal()
+                .map(principal -> {
                     if (principal instanceof JwtAuthenticationToken) {
                         Jwt jwt = ((JwtAuthenticationToken) principal).getToken();
-                        SecurityContext context = SecurityContextHolder.createEmptyContext();
-                        context.setAuthentication(new JwtAuthenticationToken(jwt));
-                        SecurityContextHolder.setContext(context);
+                        // FIXME: revisar el nombre del campo de identificación que viene en jwt
+                        return jwt.getClaims().get("asd");
                     }
-                    return chain.filter(exchange);
-                });*/
+                    return Mono.error(new BadCredentialsException("Invalid token"));
+                })
+                .map(idUser -> {
+                    //FIXME: hacer la búsqueda por id de usuario en cognito
+                    User user = this.getUserRepository.get(idUser.toString());
+
+                    return exchange
+                            .mutate()
+                            .request(builder ->
+                                    builder
+                                            .header("id-user", user.getId().toString()))
+                            .build();
+                })
+                .flatMap(chain::filter);
     }
 }
--- src/main/java/ar/germin/api/configuration/SecurityConfig.java
+++ src/main/java/ar/germin/api/configuration/SecurityConfig.java
@@ -2,7 +2,6 @@ package ar.germin.api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -19,6 +18,7 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/api/v1/auth/login").permitAll()
+                        .pathMatchers("/api/v1/auth/signup").permitAll()
                         .anyExchange().authenticated())
                 .oauth2ResourceServer(oauth2ResourceServer ->
                         oauth2ResourceServer
