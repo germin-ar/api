ff01787d146e2986773921b5ec5eac0c25f4b2d9
Diego Navarrete <diego.navarrete92@gmail.com>
2024-07-09 16:58:24 -0300

    exceptions handler

--- src/main/java/ar/germin/api/configuration/ApiExceptionHandler.java
+++ src/main/java/ar/germin/api/configuration/ApiExceptionHandler.java
@@ -1,2 +1,48 @@
-package ar.germin.api.configuration;public class ApiExceptionHandler {
+package ar.germin.api.configuration;
+
+import ar.germin.api.application.exceptions.ApplicationException;
+import ar.germin.api.application.exceptions.DiseaseCandidateNotFoundException;
+import ar.germin.api.application.exceptions.FileImageNotFoundException;
+import ar.germin.api.application.exceptions.GardenNotFoundException;
+import ar.germin.api.application.exceptions.PlantNotFoundException;
+import ar.germin.api.application.exceptions.UserCognitoNotFoundException;
+import ar.germin.api.application.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.time.OffsetDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler({
+            PlantNotFoundException.class,
+            UserNotFoundException.class,
+            GardenNotFoundException.class,
+            DiseaseCandidateNotFoundException.class,
+            FileImageNotFoundException.class,
+            UserCognitoNotFoundException.class,
+    })
+    public ProblemDetail handleNotFoundException(ApplicationException ex, ServerWebExchange exchange) {
+        return buildProblemDetail(HttpStatus.NOT_FOUND, ex, exchange);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ProblemDetail handleDefault(RuntimeException ex, ServerWebExchange exchange) {
+        return buildProblemDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex, exchange);
+    }
+
+    private ProblemDetail buildProblemDetail(HttpStatus httpStatus,
+                                             Exception ex,
+                                             ServerWebExchange exchange) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(httpStatus);
+        problemDetail.setType(exchange.getRequest().getURI());
+        problemDetail.setDetail(ex.getMessage());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("timestamp", OffsetDateTime.now());
+        return problemDetail;
+    }
 }
